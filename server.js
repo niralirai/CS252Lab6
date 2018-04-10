f2bc2cde589e77e7cc12472c427862c6d5f9e5be
Kai Hoffman <hoffm154@purdue.edu>
2018-04-09 21:33:08 -0400

    Redo routing based on new paths

--- server.js
+++ server.js
@@ -1,38 +1,46 @@
+/**
+ * References:
+ *  - https://github.com/expressjs/body-parser
+ *  - https://stackoverflow.com/questions/13337288/expressjs-sending-a-file-from-parent-directory/22336660
+ *  - https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
+ */
+
+// Express is better for handling routing
 var express = require('express');
 var app = express();
 var port = 4200;
-//express is better for handling routing
 
 var path = require('path');
 
-
 var bodyParser = require('body-parser');
+app.use(bodyParser.json());  // Parses as json and puts object into req.body
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser.json()); //parses as json and puts object into req.body
-app.use(bodyParser.urlencoded({extended: true}));//https://github.com/expressjs/body-parser
+// TODO - other html pages as needed
 
-// Send GET request and route to splash page
+// Send GET request and route to the splash page
 app.get('/', function(request, response) {
-  response.sendFile("client/src/app/splash/splash.component.html", {'root': '../'});
+  response.sendFile("splash.html", {'root' : './'});
+  console.log(request.headers);
+});
+
+// Send GET request and route to the main page
+app.get('/main', function(request, response) {
+  response.sendFile("mainPage.html", {'root' : './'});
   console.log(request.headers);
 });
 
 // TODO Send POST request (for security) and route to login page
 app.get('/login', function(request, response) {
-  response.sendFile("client/src/app/login/login.component.html", {'root': '../'});
+  response.sendFile("login.html", {'root' : './'});
   console.log(request.headers);
 });
 
 // TODO Send POST request (for security) and route to signup page
 app.get('/signup', function(request, response) {
-  response.sendFile("client/src/app/signup/signup.component.html", {'root': '../'});
+  response.sendFile("signup.html", {'root' : './'});
   console.log(request.headers);
 });
-
-//TODO other html pages
-//app.get('other.html',function(request,response){
-
-//}); 
 
 ////////This stuff can be ignored I was figuring things out//////////
 //app.use((request, response, next) => {
@@ -51,6 +59,6 @@ app.get('/signup', function(request, response) {
 //  });
 // });
 ////////////////////////////////////////
+
 app.listen(port);
 console.log("Server started on port " + port);
-
