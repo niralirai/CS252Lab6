bf6095fad02d57e842ec9ddc570748a98f9321a7
Kai Hoffman <hoffm154@purdue.edu>
2018-04-11 02:15:31 -0400

    Send POST request on form submission and handle in server.js; use .ejs in place of .html

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  *  - https://github.com/expressjs/body-parser
  *  - https://stackoverflow.com/questions/13337288/expressjs-sending-a-file-from-parent-directory/22336660
  *  - https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
+ *  - https://expressjs.com/en/advanced/developing-template-engines.html
  */
 
 // Express is better for handling routing
@@ -10,100 +11,88 @@ var express = require('express');
 var app = express();
 var port = 4200;
 
+// Parses as json and puts object into req.body
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());  // Parses as json and puts object into req.body
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static(__dirname));
 
-//var admin = require('firebase-admim');
-//admin.initializeApp({
-//  databaseURL:
-//});
+// Set views directory and template engine
+app.set('views', './views');
+app.set('view engine', 'ejs')
 
-/*var config = {
-    apiKey: "AIzaSyAKfbDYJM_6rEnYqBgGbQrR5lNqtffjYR0",
-    authDomain: "cs252lab6-7604e.firebaseapp.com",
-    databaseURL: "https://cs252lab6-7604e.firebaseio.com",
-    projectId: "cs252lab6-7604e",
-    storageBucket: "cs252lab6-7604e.appspot.com",
-    messagingSenderId: "454352118655"
-  };
-  firebase.initializeApp(config);
-*/
+/**
+ * GET requests sent when routing to the page
+ *  - / --> splash.ejs
+ *  - /main --> main.ejs
+ *  - /login --> login.ejs
+ *  - /signup --> signup.ejs
+ */
+
 // TODO - other html pages as needed
-
-// Send GET request and route to the splash page
 app.get('/', function(request, response) {
-  response.sendFile("splash.html", {'root' : './'});
+  response.render("splash");
+  console.log("GET /");
   console.log(request.headers);
+  console.log("\n");
 });
 
-// Send GET request and route to the main page
 app.get('/main', function(request, response) {
-  response.sendFile("mainPage.html", {'root' : './'});
+  response.render("main");
+  console.log("GET /main");
   console.log(request.headers);
+  console.log("\n");
 });
 
-// TODO Send POST request (for security) and route to login page
 app.get('/login', function(request, response) {
-  response.sendFile("login.html", {'root' : './'});
+  response.render("login");
+  console.log("GET /login");
   console.log(request.headers);
+  console.log("\n");
 });
 
-// TODO Send POST request (for security) and route to signup page
 app.get('/signup', function(request, response) {
-  response.sendFile("signup.html", {'root' : './'});
+  response.render("signup");
+  console.log("GET /signup");
   console.log(request.headers);
+  console.log("\n");
 });
 
-////////This stuff can be ignored I was figuring things out//////////
-//app.use((request, response, next) => {
-  //print the server started and the headers
- // console.log("Server started on port" + port);
- // console.log("Headers: ");
- // console.log(request.headers);
- // next();
-//});
-//app.use((request, response, next) => {
-  //next();
-//});
-//app.get('/', (request, response) => {
-//  response.json({
-   //maybe we need this for someting?
-//  });
-// });
-////////////////////////////////////////
+/**
+ * POST requests sent when submitting a form
+ *  - /login
+ *  - /signup
+ * TODO - reroute to correct pages and make sure URL follows suit
+ */
+
+app.post('/login', function(request, response) {
+  console.log("POST /login");
+  console.log(request.body);
+  console.log(request.headers);
+  console.log("\n");
+
+  response.send("Check credentials in the database.");
+});
+
+app.post('/signup', function(request, response) {
+  console.log("POST /signup");
+  console.log(request.body);
+  console.log(request.headers);
+  console.log("\n");
+
+  let firstname = request.body.firstname;
+  let lastname = request.body.lastname;
+  let email = request.body.email;
+  let password = request.body.password;
+  let password2 = request.body.password2;
+
+  if (password === password2) {
+    response.send("Password and re-typed password match! Reroute accordingly. :)");
+  } else {
+    response.send("Password and re-typed password do not match! Reroute accordingly. :(");
+  }
+});
 
 app.listen(port);
 console.log("Server started on port " + port);
-
-
-/*
-const http = require('http');
-var routeHandler = require('./routeHandler.js');
-
-var server = module.exports = http.createServer((request, response) => {
-  var parsedUrl = request.url.substring(1).split('/');
-  console.log(parsedUrl[0])
-  var routeFunction = routeHandler[parsedUrl[0]];
-  if(request.headers.origin){
-    response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200", 'always');
-  }
-  response.setHeader('Access-Control-Allow-Headers', 'content-type');
-  response.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  try{
-    if(request.method == 'OPTIONS'){
-      response.end("{}");
-    }else{
-      routeFunction(request,response, parsedUrl);
-    }
-  }catch(err){
-    console.error(err);
-  }
-})
-
-*/
-
-
-
-
