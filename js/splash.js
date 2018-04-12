10efa7604b3934df31cc6de51d8d1d891e76da65
Kai Hoffman <hoffm154@purdue.edu>
2018-04-12 00:55:56 -0400

    Don't route to .html files in splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,7 +1,7 @@
 function signup() {
-  window.location.href = "signup.html";
+  window.location.href = "signup";
 }
 
 function login() {
-  window.location.href = "login.html";
+  window.location.href = "login";
 }
